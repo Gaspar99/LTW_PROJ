e909128f2c18a7fa7fddfc030138eb915103661d
Pasgas99 <pasgas99@hotmail.com>
2019-10-25 12:49:17 +0100

    Changed uml

--- dataBase.sql
+++ dataBase.sql
@@ -1,6 +1,7 @@
 CREATE TABLE user (
-    usr_username VARCHAR PRIMARY KEY,
-    usr_password VARCHAR NOT NULL
+    usr_id INTEGER PRIMARY KEY
+    usr_username VARCHAR  NOT NULL,
+    password VARCHAR NOT NULL
 );
 
 CREATE TABLE apartment (
@@ -8,4 +9,16 @@ CREATE TABLE apartment (
     title VARCHAR NOT NULL,
     price_per_day REAL NOT NULL,
     location VARCHAR NOT NULL,
+);
+
+CREATE TABLE revervation (
+    apartment_id INTEGER REFERENCES apartment PRIMARY KEY,
+    user_id INTEGER REFERENCES user PRIMARY KEY,
+    initial_date VARCHAR NOT NULL,
+    final_date VARCHAR NOT NULL
+);
+
+CREATE TABLE rental (
+    usr_id INTEGER REFERENCES user PRIMARY KEY,
+    apartment_id INTEGER REFERENCES apartment NOT NULL
 );
