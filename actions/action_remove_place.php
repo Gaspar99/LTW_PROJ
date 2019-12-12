af68654d5166d13e532010c217cb367c4f07e6c6
Pasgas99 <pasgas99@hotmail.com>
2019-12-12 18:58:18 +0000

    Remove place done

--- actions/action_remove_place.php
+++ actions/action_remove_place.php
@@ -6,8 +6,8 @@ include_once("../database/db_places.php");
 $user_id = get_user_id($_SESSION["user_email"]);
 $place_id = $_POST["place_id"];
 
-remove_place($place_id);
 remove_place_photos($place_id); 
+remove_place($place_id);
 
 header("Location: ../pages/profile.php?id=$user_id");
 
