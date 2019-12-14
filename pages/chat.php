986892f38e45e43a250fd7ac0d01f7272fad9f5a
José Carlos Magalhaes <gustavonrm@gmail.com>
2019-12-14 17:59:48 +0000

    missing file + search bug

---
+++ pages/chat.php
@@ -0,0 +1,9 @@
+<?php
+include_once("../includes/session.php");
+include_once("../templates/tpl_common.php");
+#get_messages 
+
+draw_site_header();
+draw_title("My messages");
+#draw messages
+draw_footer();
