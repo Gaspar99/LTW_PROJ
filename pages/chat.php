ade88f171753426f13570d7b0b6100fa0f29d814
Gustavo nRm <gustavonrm@gmail.com>
2019-12-15 19:56:42 +0000

    starting chat

--- pages/chat.php
+++ pages/chat.php
@@ -1,9 +1,10 @@
 <?php
 include_once("../includes/session.php");
 include_once("../templates/tpl_common.php");
+include_once("../templates/tpl_chat.php");
 #get_messages 
 
 draw_site_header();
 draw_title("My messages");
-#draw messages
+draw_chat();
 draw_footer();
---
+++ templates/tpl_chat.php
@@ -0,0 +1,21 @@
+<?php 
+
+function draw_chat(){?>
+    <div><?php
+        //draw_users_to_message();
+        draw_chat_box();
+    ?></div>
+<?php } 
+
+function draw_chat_box(){?>
+    <div id="chat_box">
+        olaa
+    </div>
+<?php }
+
+function draw_users_to_message(){
+
+}
+
+?>
+
